5a6e04e8234770f471ab942e3348a55d23d56277
lPatrickll <patrickgonzalesquintana@gmail.com>
2025-06-13 10:41:00 -0400

    refactoring: se agrego punto y coma y la validacion exacta en los caminos

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,7 @@
 export default function fizzbuzz(n) {
-  if (n == 3)
-    return "fizz"
-  if (n == 5)
-    return "buzz"
+  if (n === 3)
+    return "fizz";
+  if (n === 5)
+    return "buzz";
   return n + "";
 }
