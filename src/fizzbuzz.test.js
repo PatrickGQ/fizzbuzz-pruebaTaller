8c2bd7fa2665aee1bdb2b6eaeee3fe2bb4d678ec
lPatrickll <patrickgonzalesquintana@gmail.com>
2025-06-13 10:28:43 -0400

    Se cambio la funcion para q devolviera un string

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,9 +1,13 @@
 describe("FizzBuzz", () => {
   it("deberia mostrar un numero n", () => {
-    expect(fizzbuzz(1)).toEqual(1);
+    expect(fizzbuzz(1)).toEqual("1");
+  });
+
+  it("deberia mostrar el 2", () => {
+    expect(fizzbuzz(2)).toEqual("2");
   });
 });
 
 function fizzbuzz(n) {
-  return n;
+  return n + "";
 }
